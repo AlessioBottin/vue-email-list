2f50496cb5dcd31dd1d4f906fcd80cf231112bb9
AlessioBottin <alessiobottin2002@gmail.com>
2021-12-15 17:05:20 +0100

    for

--- js/script.js
+++ js/script.js
@@ -12,13 +12,13 @@ const app = new Vue(
         },
         methods: {
             getMail: function() {
-                while (this.mailArray.length < 10) {
+                for (let i = 0; i < 10; i++) {
 
                     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then((response) => {
                         this.mailArray.push(response.data.response);
                     });
-                };
+                }
                 console.log(this.mailArray);
             }
         },
